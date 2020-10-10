7a71d3dc14845ddf8ca8d83f3ff96c66503051a8
Neto Costa <neto_costa_1809@hotmail.com>
2020-10-09 23:33:05 -0300

    Complete - News

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,52 @@
 import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import Router from 'vue-router';
+import store from '../store/store';
 
-Vue.use(VueRouter);
+import TopStories from '../views/TopStories.vue';
+import CodeExamples from '../views/CodeExamples.vue';
+import MyFavorites from '../views/MyFavorites.vue';
 
-const routes: RouteConfig[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
+Vue.use(Router);
 
-const router = new VueRouter({
+class RouteMeta {
+  title: string;
+
+  constructor({title}: { title: string }) {
+    this.title = title;
+  }
+}
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'top-stories',
+      component: TopStories,
+      meta: new RouteMeta({ title: 'Top Stories' })
+    },
+    {
+      path: '/code-examples',
+      name: 'code-examples',
+      component: CodeExamples,
+      meta: new RouteMeta({ title: 'Code Examples' })
+    },
+    {
+      path: '/my-favorites',
+      name: 'my-favorites',
+      component: MyFavorites,
+      meta: new RouteMeta({ title: 'Favorites' })
+    }
+  ]
+});
+
+// This callback runs before every route change, including on initial load
+router.beforeEach((to, from, next) => {
+
+  const routeMeta = to.meta as RouteMeta;
+  store.dispatch('topToolbar/changeTitle', routeMeta.title);
+  next();
 });
 
 export default router;
